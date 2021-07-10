c04e6abbb4cd1f7dcb211d34a3d27be687bb02d3
SrinathNarayan <78892115+SrinathNarayan@users.noreply.github.com>
2021-07-10 11:42:04 +0530

    Day 17 Commit

--- Day37/src/main/java/SwingComponents/MenUDrivenExample.java
+++ Day37/src/main/java/SwingComponents/MenUDrivenExample.java
@@ -1,38 +1,56 @@
 package SwingComponents;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Scanner;
 
-public class MenUDrivenExample {
-	 public static void main(String[] args) {
-		  
-		  int choice, num;
-		  Scanner n = new Scanner(System.in);
-		  System.out.println("Menu....");
-		  System.out.println("1. Find positive");
-		  System.out.println("2. Odd/Even");
-		  System.out.println("Enter the choice : ");
-		  choice = n.nextInt();
-		 
-		  switch(choice) {
-		  
-		   case 1:
-		    System.out.println("Enter number : ");
-		    num = n.nextInt();
-		    if(num > 0)
-		     System.out.println("Positive...");
-		    else
-		     System.out.println("Negative...");
-		    break;
-		   case 2:
-		    System.out.println("Enter number : ");
-		    num = n.nextInt();
-		    if(num%2 == 0)
-		     System.out.println("Even...");
-		    else
-		     System.out.println("Odd...");
-		    break;
-		   default:
-		    System.out.println("Wrong choice..");
-		  }
-		 }
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JTextArea;
+
+public class MenUDrivenExample implements ActionListener{    
+	JFrame f;    
+	JMenuBar mb;    
+	JMenu file,edit,help;    
+	JMenuItem cut,copy,paste,selectAll;    
+	JTextArea ta;    
+	MenUDrivenExample(){    
+	f=new JFrame();    
+	cut=new JMenuItem("cut");    
+	copy=new JMenuItem("copy");    
+	paste=new JMenuItem("paste");    
+	selectAll=new JMenuItem("selectAll");    
+	cut.addActionListener(this);    
+	copy.addActionListener(this);    
+	paste.addActionListener(this);    
+	selectAll.addActionListener(this);    
+	mb=new JMenuBar();    
+	file=new JMenu("File");    
+	edit=new JMenu("Edit");    
+	help=new JMenu("Help");     
+	edit.add(cut);edit.add(copy);edit.add(paste);edit.add(selectAll);    
+	mb.add(file);mb.add(edit);mb.add(help);    
+	ta=new JTextArea();    
+	ta.setBounds(5,5,360,320);    
+	f.add(mb);f.add(ta);    
+	f.setJMenuBar(mb);  
+	f.setLayout(null);    
+	f.setSize(400,400);    
+	f.setVisible(true);    
+	}     
+	public void actionPerformed(ActionEvent e) {    
+	if(e.getSource()==cut)    
+	ta.cut();    
+	if(e.getSource()==paste)    
+	ta.paste();    
+	if(e.getSource()==copy)    
+	ta.copy();    
+	if(e.getSource()==selectAll)    
+	ta.selectAll();    
+	}     
+	public static void main(String[] args) {    
+	    new MenUDrivenExample();    
+	}    
 }
